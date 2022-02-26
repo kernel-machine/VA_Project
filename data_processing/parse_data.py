a0c3ab0f6fb889d9e14cd4e80cc45c8db8d2adc8
Luca Giovannesi <l.giovannesi@gmail.com>
2022-02-26 19:14:23 +0100

    New json format

--- data_processing/parse_data.py
+++ data_processing/parse_data.py
@@ -1,16 +1,20 @@
 import ast
-import math
 
 import pandas as pd
 import json
 from math import isnan
 import multiprocessing
+import os
 
 movies_metadata = pd.read_csv("kaggle/movies_metadata.csv", low_memory=False)
 keywords_csv = pd.read_csv("kaggle/keywords.csv", low_memory=False)
 
 print("Number of rows", len(movies_metadata))
 print("Number of rows of keywords", len(keywords_csv))
+
+manager = multiprocessing.Manager()
+shared_list = manager.list()
+progress_list = manager.list()
 
 
 def get_keywords_by_id(id):
@@ -20,21 +24,17 @@ def get_keywords_by_id(id):
     return None
 
 
-manager = multiprocessing.Manager()
-moviesDict = manager.dict()
-
-
-def process_chuck(thread_id, start_row, end_row, dict):
+def process_chuck(thread_id, start_row, end_row, result, progress):
     for current_row in range(start_row, end_row):
         try:
-            id = movies_metadata.at[current_row, 'id']
+            movie_id = int(movies_metadata.at[current_row, 'id'])
             imdb_id = movies_metadata.at[current_row, 'imdb_id']
             title = movies_metadata.at[current_row, 'title']
             genres = ast.literal_eval(movies_metadata.at[current_row, 'genres'])
             release_data = movies_metadata.at[current_row, 'release_date']  # Format 1989-02-17
             runtime = float(movies_metadata.at[current_row, 'runtime'])
             spoken_languages = ast.literal_eval(movies_metadata.at[current_row, 'spoken_languages'])
-            keywords = ast.literal_eval(get_keywords_by_id(id))
+            keywords = ast.literal_eval(get_keywords_by_id(movie_id))
             vote_average = float(movies_metadata.at[current_row, 'vote_average'])
             vote_count = int(movies_metadata.at[current_row, 'vote_count'])
             revenue = int(movies_metadata.at[current_row, 'revenue'])
@@ -45,7 +45,8 @@ def process_chuck(thread_id, start_row, end_row, dict):
         else:
             if vote_average > 0 and vote_count > 0 and revenue > 0 and popularity > 0 and budget > 0 and \
                     keywords is not None and not isnan(runtime):
-                dict[id] = {
+                result.append({
+                    'id': movie_id,
                     'imdb_id': imdb_id,
                     'title': title,
                     'genres': genres,
@@ -58,24 +59,31 @@ def process_chuck(thread_id, start_row, end_row, dict):
                     'popularity': popularity,
                     'budget': budget,
                     'keywords': keywords
-                }
-                if current_row % 10 == 0:
-                    process_count = (current_row - start_row) / (end_row - start_row)
-                    print("TR", thread_id, "\tProgress:", int(process_count * 100), "%")
+                })
+                # Update progress status
+                progress[thread_id] = int(100 * (current_row - start_row) // (end_row - start_row))
+                if current_row % 100 == 0:
+                    # Print progresses of the threads
+                    print("PROGRESS", " ".join(list(map(lambda x: str(x) + "%", progress))))
 
-    print("TR", thread_id, "\tEND")
+    progress[thread_id] = 100
 
 
 cpu_cores = multiprocessing.cpu_count()
 chuck_size = len(movies_metadata) // cpu_cores
 processes = []
 for i in range(cpu_cores):
-    p = multiprocessing.Process(target=process_chuck, args=(i, chuck_size * i, chuck_size * (i + 1), moviesDict))
+    progress_list.append(0)
+    p = multiprocessing.Process(target=process_chuck,
+                                args=(i, chuck_size * i, chuck_size * (i + 1), shared_list, progress_list))
     processes.append(p)
 
 if len(movies_metadata) > chuck_size * cpu_cores:
+    progress_list.append(0)
     p = multiprocessing.Process(target=process_chuck,
-                                args=(cpu_cores, chuck_size * cpu_cores, len(movies_metadata), moviesDict))
+                                args=(
+                                    cpu_cores, chuck_size * cpu_cores, len(movies_metadata), shared_list,
+                                    progress_list))
     processes.append(p)
 
 for p in processes:
@@ -84,6 +92,12 @@ for p in processes:
 for p in processes:
     p.join()
 
-print("Movies size", len(moviesDict))
+movies = list(shared_list)
+
+print("Sorting")
+movies.sort(key=lambda x: x['id'])
+moviesDict = {'movies': movies}
+
+print("Movies size", len(moviesDict['movies']))
 with open('dataset.json', 'w') as outfile:
-    json.dump(moviesDict.copy(), outfile, allow_nan=False, indent=1)
+    json.dump(moviesDict, outfile, allow_nan=False, indent=1)
