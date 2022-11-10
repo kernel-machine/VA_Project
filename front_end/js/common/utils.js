fe0c2378b059d05219190026bb9538e782a2fa1a
Luca Giovannesi <l.giovannesi@gmail.com>
2022-11-10 12:08:56 +0100

    Zoom implemented

--- front_end/js/common/utils.js
+++ front_end/js/common/utils.js
@@ -5,3 +5,16 @@ export function range(start, end, step=1){
     
         return ris
 }
+
+export function tickValuesFormatter(value) {
+    let strValue
+    if (value / 1000000000 > 1)
+        strValue = (value / 1000000000).toFixed(1) + "B"
+    else if (value / 1000000 > 1)
+        strValue = (value / 1000000).toFixed(1) + "M"
+    else if (value / 1000 > 1)
+        strValue = (value / 1000).toFixed(1) + "K"
+    else
+        strValue = value.toFixed(1)
+    return strValue.replace(/\.0/, '');
+}
