db39107125cf2f0b7d6070f408e7179b7f9f164c
Luca Giovannesi <l.giovannesi@gmail.com>
2022-11-14 19:30:53 +0100

    Loading screen

--- front_end/js/main.js
+++ front_end/js/main.js
@@ -3,12 +3,13 @@ import {BubblePlot} from "./graphs/bubblePlot.js";
 import {ColumnPlot} from "./graphs/columnPlot.js";
 import {SelectionTool} from "./common/selectionTool.js";
 import {MDSGraph} from "./graphs/mdsGraph.js";
-import { SelectionList} from "./selectionList.js";
+import {SelectionList} from "./selectionList.js";
 
 const DATASET_PATH = "./resources/dataset/dataset.json"
 
 function init() {
     const selectionTool = new SelectionTool();
+
     d3.json(DATASET_PATH)
         .then((response) => {
             const movies = response.movies
@@ -24,6 +25,15 @@ function init() {
             parallelCoordinates.selectionTool = selectionTool;
             bubblePlot.selectionTool = selectionTool;
             mdsGraph.selectionTool = selectionTool;
+            document.getElementById("loadingDiv").style.display = "none"
+            let opacity = 0
+            const intervalId = setInterval(() => {
+                opacity += 0.2
+                document.getElementById("mainContainer").style.opacity = opacity
+                if (opacity >= 1)
+                    clearInterval(intervalId)
+            }, 50)
+
         })
 }
 
